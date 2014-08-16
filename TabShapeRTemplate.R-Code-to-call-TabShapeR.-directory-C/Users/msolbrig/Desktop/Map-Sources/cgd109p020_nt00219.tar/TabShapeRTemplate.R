8e547c82dbea0a0758319121332d607115ad29e4
msolbrig <msolbrig@gmail.com>
2014-08-15 20:51:49 -0700

    Create TabShapeRTemplate.R
    A template for calling TabShapeR code in R

---
+++ TabShapeRTemplate.R-Code-to-call-TabShapeR.-directory-C/Users/msolbrig/Desktop/Map-Sources/cgd109p020_nt00219.tar/TabShapeRTemplate.R
@@ -0,0 +1,10 @@
+#Example code for calling TabShapeR commands in R
+
+directory = <Enter path name Here>
+name = <Enter name of shapefile here>
+endName = <Enter name of file to be written here>
+
+#Delete the # in front of the appropriate command, then run code.  
+#TabShapeRLines(directory,name,endName)
+#TabShapeRPoints(directory,name,endName)
+#TabShapeRPolygons(directory,name,endName)
